90878e2d1911cecbb0d6d594e56ec1c793e90c08
Caesar <ushmanp@gmail.com>
2023-10-02 09:00:24 +0100

    Done

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,3 +1,65 @@
+// 'use client';
+
+// import { useState, useEffect } from "react";
+// import PromptCard from "./PromptCard";
+
+// const PromptCardList = ({ data, handleTagClick }) => {
+//   return (
+//     <div className="mt-16 prompt_layout">
+//       {data.map((post) => (
+//         <PromptCard
+//          key={post._id}
+//          post={post}
+//          handleTagClick={handleTagClick}
+//         />
+//       ))}
+//     </div>
+//   )
+// }
+
+// const Feed = () => {
+//   const [searchText, setSearchText] = useState('');
+
+//   const [posts, setPosts] = useState([]);
+
+//   const handleSearchChange = (e) => {}
+
+//   useEffect(() => {
+//     const fetchPosts = async () => {
+//       const response = await fetch('/api/prompt');
+//       const data = await response.json();
+
+//       setPosts(data);
+//     }
+
+//     console.log(posts);
+
+//     fetchPosts();
+//   }, []);
+
+//   return (
+//     <section className="feed">
+//       <form className="relative w-full flex-center">
+//         <input 
+//          type="text"
+//          placeholder="Search for a tag or a username"
+//          value={searchText}
+//          onChange={handleSearchChange}
+//          required
+//          className="search_input peer"
+//         />
+//       </form>
+
+//       <PromptCardList
+//        data={posts}
+//        handleTagClick={() =>{}}
+//       />
+//     </section>
+//   );
+// }
+
+// export default Feed;
+
 'use client';
 
 import { useState, useEffect } from "react";
@@ -8,9 +70,9 @@ const PromptCardList = ({ data, handleTagClick }) => {
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
         <PromptCard
-         key={post._id}
-         post={post}
-         handleTagClick={handleTagClick}
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
         />
       ))}
     </div>
@@ -19,10 +81,11 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
-
   const [posts, setPosts] = useState([]);
 
-  const handleSearchChange = (e) => {}
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  }
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -32,30 +95,56 @@ const Feed = () => {
       setPosts(data);
     }
 
-    console.log(posts);
-
     fetchPosts();
   }, []);
+
+  // Search by ID
+  const searchById = (id) => {
+    return posts.filter((post) => post._id === id);
+  }
+
+  // Search by Tag
+  const searchByTag = (tag) => {
+    return posts.filter((post) => post.tags.includes(tag));
+  }
+
+  // Search by Username
+  const searchByUsername = (username) => {
+    return posts.filter((post) => {
+      return (
+        post.username &&
+        post.username.toLowerCase() === username.toLowerCase()
+      );
+    });
+  }
+
+  // Apply the appropriate search based on searchText format
+  const filteredPosts = searchText.trim().startsWith('#')
+    ? searchByTag(searchText.trim().substring(1))
+    : searchText.trim().length === 24 && searchText.match(/^[0-9a-fA-F]+$/)
+    ? searchById(searchText.trim())
+    : searchByUsername(searchText.trim());
 
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
         <input 
-         type="text"
-         placeholder="Search for a tag or a username"
-         value={searchText}
-         onChange={handleSearchChange}
-         required
-         className="search_input peer"
+          type="text"
+          placeholder="Search for a tag, username, or ID"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
         />
       </form>
 
       <PromptCardList
-       data={posts}
-       handleTagClick={() =>{}}
+        data={filteredPosts}
+        handleTagClick={() => {}}
       />
     </section>
   );
 }
 
 export default Feed;
+
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -32,7 +32,7 @@ const Nav = () => {
          height={30}
          className="object-contain" 
         />
-        <p className="logo_text">Promptopia</p>
+        <p className="logo_text">Prompy</p>
       </Link>
 
 
